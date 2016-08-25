68007903555d6ab797134d71c96a3be310847e06
CEITELABINFO <ceitelabinfo@adserver.unisantos.br>
2016-08-24 22:17:23 -0300

    Desenvolvendo main

--- Cesar/Cesar/main.c
+++ Cesar/Cesar/main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void code_cesar(char[], int);
+void decode_cesar(char[], int);
+
 int main()
 {
-    printf("Hello world!\n");
+    char frase;
+    int dec, pos=3,i,n=0;
+
+    printf("Insira a frase:\n");
+
+    scanf("%s", frase);
+
+    printf("Insira 1 para codificar\n, 2 para decodificar\n, 3 para encerrar:\n");
+    scanf("%d", dec);
+
+    while (dec=!3)
+    {
+
+        switch (dec)
+        {
+        case (1):
+            for (i=0, frase[i]=! '\0', i++){
+                code_cesar(frase[n], pos);
+                n++;
+            }
+
+        case (2):
+            for (i=0, frase[i]=! '\0', i++){
+                decode_cesar(frase[n], pos);
+                n++;
+            }
+
+        case (3):
+            printf("%s", frase);
+        }
+    }
+
     return 0;
 }
