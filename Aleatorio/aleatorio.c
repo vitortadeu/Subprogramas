08ced38b00fe11af7a8d1cd278a616b1483cedc5
CEITELABINFO <ceitelabinfo@adserver.unisantos.br>
2016-08-26 20:02:06 -0300

    Estruturacao main

--- Aleatorio/aleatorio.c
+++ Aleatorio/aleatorio.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+//Caracteres aleatorios
 
+/*2)	Crie uma função que recebe uma string e transforma alguns dos caracteres em
+maiúsculos e outros em minúsculos. Faça sorteios com a função rand() para gerar
+números aleatórios em C, que serão usados para escolher os índices dos caracteres
+que serão alterados. Como parâmetro deve ser recebida a quantidade de sorteios que
+devem ser executados  */
+
+void transforma(char [], int);
+
+int main(){
+
+    char frase;
+    int num;
+
+    printf("Insira a frase:");
+    scanf("%[^\n]s", frase);
+
+    printf("Insira a quantidade de sorteios:");
+    scanf("%d", num);
+
+    transforma(frase, num);
+
+}
+
+void transforma(char frase[], int qtde_sorteios){
+
+    printf("%s", frase);
+}
